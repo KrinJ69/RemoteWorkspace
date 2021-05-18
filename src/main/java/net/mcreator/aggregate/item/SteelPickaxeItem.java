e2bdd9c0a0c1b82afd6ed532cab82da154ce3dc6
KrinJ69 <User@DESKTOP-I6B0VF3>
2021-05-18 15:15:18 +0200

    KrinJ - Tools Added

---
+++ src/main/java/net/mcreator/aggregate/item/SteelPickaxeItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.aggregate.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.aggregate.AggregateModElements;
+
+@AggregateModElements.ModElement.Tag
+public class SteelPickaxeItem extends AggregateModElements.ModElement {
+	@ObjectHolder("aggregate:steel_pickaxe")
+	public static final Item block = null;
+	public SteelPickaxeItem(AggregateModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 300;
+			}
+
+			public float getEfficiency() {
+				return 7f;
+			}
+
+			public float getAttackDamage() {
+				return 1f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 14;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(SteelIngotItem.block, (int) (1)));
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("steel_pickaxe"));
+	}
+}
